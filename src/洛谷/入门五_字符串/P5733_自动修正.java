0c6cb72e1519190710bf9fd30747d7c95a53790c
Lonely_Snow <3200975318@qq.com>
2023-11-14 22:12:52 +0800

    normalization submit 23/11/14

---
+++ src/洛谷/入门五_字符串/P5733_自动修正.java
@@ -0,0 +1,11 @@
+package 洛谷.入门五_字符串;
+
+import java.util.Scanner;
+
+public class P5733_自动修正 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str  = sc.nextLine();
+        System.out.println(str.toUpperCase());
+    }
+}
